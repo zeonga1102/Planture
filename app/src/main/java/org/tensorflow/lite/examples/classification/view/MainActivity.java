94b7ee0a377caf6ca8c47828e19907e58cf60151
CherichK <ksy0898@naver.com>
2021-05-06 17:53:51 +0900

    floating button 추가, lib_support->assets 추가

--- app/src/main/java/org/tensorflow/lite/examples/classification/view/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/view/MainActivity.java
@@ -3,15 +3,28 @@ package org.tensorflow.lite.examples.classification.view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.tensorflow.lite.examples.classification.ClassifierActivity;
 import org.tensorflow.lite.examples.classification.R;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
+
+    private Animation fab_open, fab_close;
+    private Boolean isFabOpen = false;
+    private FloatingActionButton mainFloating, cameraFloating, galleryFloting, textFloating;
+
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -21,6 +34,23 @@ public class MainActivity extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //가로화면 안됨.
 
+        context = this;
+
+        //floating action button==================================================
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+
+        mainFloating = (FloatingActionButton)findViewById(R.id.mainFloating);
+        cameraFloating = (FloatingActionButton)findViewById(R.id.cameraFloating);
+        galleryFloting =(FloatingActionButton)findViewById(R.id.galleryFloating);
+        textFloating = (FloatingActionButton)findViewById(R.id.textFloating);
+
+        mainFloating.setOnClickListener(onClickListener);
+        cameraFloating.setOnClickListener(onClickListener);
+        galleryFloting.setOnClickListener(onClickListener);
+        textFloating.setOnClickListener(onClickListener);
+
+        //바텀네비게이션 설정==============================================================
         bottomNavigationView = findViewById(R.id.nav_view);
 
         getSupportFragmentManager().beginTransaction().add(R.id.main_frame, new HomeFragment()).commit(); //FrameLayout에 fragment.xml 띄우기
@@ -43,4 +73,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            switch (id) {
+                case R.id.mainFloating: anim(); break;
+                case R.id.cameraFloating:
+                    anim();
+                    Intent intent = new Intent(context, ClassifierActivity.class);
+                    startActivityForResult(intent, 0);
+                    break;
+                case R.id.galleryFloating: anim(); break;
+                case R.id.textFloating: anim(); break;
+            }
+        }
+    };
+
+    public void anim() {
+        if (isFabOpen) {
+            cameraFloating.startAnimation(fab_close);
+            galleryFloting.startAnimation(fab_close);
+            textFloating.startAnimation(fab_close);
+
+            cameraFloating.setClickable(false);
+            galleryFloting.setClickable(false);
+            textFloating.setClickable(false);
+
+            isFabOpen = false;
+        } else {
+            cameraFloating.startAnimation(fab_open);
+            galleryFloting.startAnimation(fab_open);
+            textFloating.startAnimation(fab_open);
+
+            cameraFloating.setClickable(true);
+            galleryFloting.setClickable(true);
+            textFloating.setClickable(true);
+            isFabOpen = true;
+        }
+    }
+
+
 }
